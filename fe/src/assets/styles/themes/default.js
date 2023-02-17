c0d8fc66eac2fcaeead68548aa93a5dd51025521
Matheus Lopes <matheus.lopesmdev@gmail.com>
2023-02-17 00:01:29 -0300

    Criando Header do Contacts List

--- fe/src/assets/styles/themes/default.js
+++ fe/src/assets/styles/themes/default.js
@@ -1,3 +1,11 @@
 export default {
-  backgroundColor: '#F6F5FC',
+  colors: {
+    background: '#F6F5FC',
+    primary: {
+      lighter: '#E8E3FF',
+      light: '#6674F4',
+      main: '#5861FC',
+      dark: '#3346F8',
+    },
+  },
 };
