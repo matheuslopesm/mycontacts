5291feb5651afa188c7fecac7743224e5a0f99b5
Matheus Lopes <matheus.lopesmdev@gmail.com>
2023-04-05 16:34:56 -0300

    Adaptando a requisição para Async/Await

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -24,21 +24,28 @@ export default function Home() {
   )), [contacts, searchTerm]);
 
   useEffect(() => {
-    setIsLoading(true);
+    async function loadContacts() {
+      try {
+        setIsLoading(true);
 
-    fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
-      .then(async (response) => {
+        const response = await fetch(
+          `http://localhost:3001/contacts?orderBy=${orderBy}`,
+        );
+
         await delay(500);
 
         const json = await response.json();
         setContacts(json);
-      })
-      .catch((error) => {
-        console.log('erro', error);
-      })
-      .finally(() => {
+      } catch (error) {
+        console.log('error', error);
+      } finally {
         setIsLoading(false);
-      });
+      }
+    }
+
+    loadContacts();
+
+    return () => console.log('cleanup');
   }, [orderBy]);
 
   function handleToggleOrderBy() {
