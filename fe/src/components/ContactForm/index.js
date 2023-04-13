374c7bc431e1717dfb87377648e950579c5d15df
Matheus Lopes <matheus.lopesmdev@gmail.com>
2023-04-13 18:18:10 -0300

    Adicionando Loading no Select de categorias

--- fe/src/components/ContactForm/index.js
+++ fe/src/components/ContactForm/index.js
@@ -23,6 +23,7 @@ export default function ContactForm({ buttonLabel }) {
   const [phone, setPhone] = useState('');
   const [categoryId, setCategoryId] = useState('');
   const [categories, setCategories] = useState([]);
+  const [isLoadingCategories, setIsLoadingCategories] = useState(true);
 
   const {
     errors,
@@ -38,7 +39,9 @@ export default function ContactForm({ buttonLabel }) {
       try {
         const categoriesList = await CategoriesService.listCategories();
         setCategories(categoriesList);
-      } catch {}
+      } catch {} finally {
+        setIsLoadingCategories(false);
+      }
     }
 
     loadCategories();
@@ -106,10 +109,11 @@ export default function ContactForm({ buttonLabel }) {
         />
       </FormGroup>
 
-      <FormGroup>
+      <FormGroup isLoading={isLoadingCategories}>
         <Select
           value={categoryId}
           onChange={(event) => setCategoryId(event.target.value)}
+          disabled={isLoadingCategories}
         >
           <option value="">Sem categoria</option>
 
@@ -118,8 +122,8 @@ export default function ContactForm({ buttonLabel }) {
               {category.name}
             </option>
           ))}
-
         </Select>
+
       </FormGroup>
 
       <ButtonContainer>
