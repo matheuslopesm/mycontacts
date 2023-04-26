ee6597ad4f88c93e5701f8b7cc39f6cf010f92d3
Matheus Lopes <matheus.lopesmdev@gmail.com>
2023-04-26 15:47:00 -0300

    Criando o objeto de toasts

--- fe/src/components/Toast/ToastContainer/index.js
+++ fe/src/components/Toast/ToastContainer/index.js
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import { Container } from './styles';
 
 import ToastMessage from '../ToastMessage/index.js';
 
 export default function ToastContainer() {
+  const [messages] = useState([
+    { id: Math.random(), type: 'default', text: 'Default text' },
+    { id: Math.random(), type: 'danger', text: 'Danger text' },
+    { id: Math.random(), type: 'success', text: 'Success text' },
+  ]);
+
   return (
     <Container>
-      <ToastMessage text="Default toast" type="default" />
-      <ToastMessage text="Error toast" type="danger" />
-      <ToastMessage text="Success toast" type="success" />
+      {messages.map((message) => (
+        <ToastMessage
+          key={message.id}
+          type={message.type}
+          text={message.text}
+        />
+      ))}
     </Container>
   );
 }
