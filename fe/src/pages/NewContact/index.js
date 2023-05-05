e9c4cbcd3e4fe593c49e22ac120118f8449bbc3c
Matheus Lopes <matheus.lopesmdev@gmail.com>
2023-05-05 13:53:08 -0300

    Refatorando o reset do Forms na EditContact

--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,3 +1,5 @@
+import { useRef } from 'react';
+
 import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
 import ContactsService from '../../services/ContactsService';
@@ -5,6 +7,8 @@ import ContactsService from '../../services/ContactsService';
 import toast from '../../utils/toast';
 
 export default function NewContact() {
+  const contactFormRef = useRef(null);
+
   async function handleSubmit(formData) {
     try {
       const contact = {
@@ -14,6 +18,7 @@ export default function NewContact() {
         category_id: formData.categoryId,
       };
       await ContactsService.createContact(contact);
+      contactFormRef.current.resetFields();
 
       toast({
         type: 'success',
@@ -34,6 +39,7 @@ export default function NewContact() {
       />
 
       <ContactForm
+        ref={contactFormRef}
         buttonLabel="Cadastrar"
         onSubmit={handleSubmit}
       />
